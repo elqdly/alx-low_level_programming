1c007650320513c76ea16ea86da678f4ea82266b
eladly <mohmadeeladli2002@gmail.com>
2023-08-16 13:48:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - function that prints _putchar
- * Return - returns 0;
+ * main - Entry point
+ *
+ * Description: This function prints the characters of "_putchar" followed by a newline.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	return (0);
+    return (0);
 }
