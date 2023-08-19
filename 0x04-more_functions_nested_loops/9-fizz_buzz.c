606a72ee66b506024ee18864ed4a24fec390ed0e
eladly <mohmadeeladli2002@gmail.com>
2023-08-18 18:36:08 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 /**
-*main - Fizz-Buzz program
-*Return: 0;
-*/
-
+ * main - Fizz-Buzz program
+ * Return: 0
+ */
 int main(void)
 {
 int i;
 
-for (i = 1 ; i <= 100 ; i++)
+for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
-    printf("FizzBuzz ");
+printf("FizzBuzz ");
 else if (i % 3 == 0)
-    printf("Fizz ");
+printf("Fizz ");
 else if (i % 5 == 0)
-    printf("Buzz ");
+printf("Buzz ");
 else
-    printf("%d ", i);
+printf("%d ", i);
 }
 printf("\n");
 return (0);
