9af1e6c5525a689f924411bd41987c8a8aa1e7ed
eladly <mohmadeeladli2002@gmail.com>
2023-08-21 12:54:30 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - finds teh length of a string
+ * @s: String to be counted
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+int i;
+int count = 0;
+
+for (i = 0 ; s[i] != '\0' ; i++)
+	count++;
+return (count);
+}
